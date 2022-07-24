27e1d44390da29ca75a7f05f48a46ccf4bb9f51e
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-07-24 01:49:56 -0500

    chore: Arreglos MasterPageAd, Proyecto ASP

--- web form asp/Proyecto/Proyecto/MasterPageAd.Master.cs
+++ web form asp/Proyecto/Proyecto/MasterPageAd.Master.cs
@@ -13,5 +13,10 @@ namespace Proyecto
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
     }
 }
